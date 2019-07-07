4aa6846af8aa90060b88d474dea528a5da41adb0
Mashpy <mashpysays@gmail.com>
2019-07-07 23:24:14 +0600

    You can edit your meal

--- app/Http/Controllers/Admin/AdminMealOrderController.php
+++ app/Http/Controllers/Admin/AdminMealOrderController.php
@@ -3,34 +3,31 @@
 namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 
-use App\User;
-use App\Models\RoleType;
-use App\Models\UserType;
 use App\Models\MealOrder;
 use App\Models\MealRate;
-use App\Models\Department;
 use Auth;
 use Illuminate\Http\Request;
-use Illuminate\Routing\Route;
-use Illuminate\Support\Facades\Input;
-use Illuminate\Support\Facades\Redirect;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\URL;
-use Illuminate\Validation\Rule;
-use phpDocumentor\Reflection\DocBlock\Tags\See;
 use Response;
 use App\Models\Country;
 use Validator;
-use Illuminate\Support\Facades\Session;
 use App\Helpers\Helper;
-use Illuminate\Support\Facades\Mail;
+use Carbon\Carbon;
 
 class AdminMealOrderController extends Controller
 {
 
     public function index()
     {
-        $meal_orders = MealOrder::all();
+        if(request()->get('from_date')){
+            $from_date = request()->get('from_date');
+            $to_date = request()->get('to_date');
+        } else {
+            $from_date =  Carbon::now()->startOfMonth();
+            $to_date = Carbon::now()->endOfMonth();
+        }
+
+        $meal_orders = MealOrder::where('created_at', '>=', $from_date)
+            ->where('created_at', '<=', $to_date)->where('user_id', Auth::user()->id)->get();
         return view('admin.meal_order.index', compact('meal_orders'));
     }
     public function create(){
@@ -47,6 +44,23 @@ class AdminMealOrderController extends Controller
         $meal_order->meal_rate_id = $request->meal_rate_id;
         $meal_order->save();
         return redirect(route('admin.meal_order.index'));
+    }
 
+    public function edit($id){
+        $meal_order = MealOrder::find($id);
+        $meal_rates = MealRate::all();
+        return view('admin.meal_order.edit', compact('meal_order', 'meal_rates'));
+    }
+
+    public function update(Request $request, $id){
+        $user_id = Auth::user()->id;
+        $meal_order = MealOrder::find($id);
+        $meal_order->breakfast = $request->breakfast;
+        $meal_order->lunch = $request->lunch;
+        $meal_order->dinner = $request->dinner;
+        $meal_order->user_id = $user_id;
+        $meal_order->meal_rate_id = $request->meal_rate_id;
+        $meal_order->update();
+        return redirect(route('admin.meal_order.index'));
     }
 }
--- resources/views/admin/meal_order/index.blade.php
+++ resources/views/admin/meal_order/index.blade.php
@@ -1,27 +1,39 @@
 @extends('layouts.admin_master')
 
 @section('content')
-
-    <h1 class="pull-right">
-        <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{!! route('admin.meal_order.create') !!}">Add New</a>
+    <h1>
+        Meal Orders
     </h1>
+
+    <form action="{{route('admin.report.index')}}" accept-charset="UTF-8" method="get"><input name="utf8" type="hidden" value="✓">
+        From:
+        <input size="16" type="text" name="from_date" value="{{ empty(request()->get('from_date'))? \Carbon\Carbon::now()->startOfMonth()->format('d-m-Y') : request()->get('from_date')->format('d-m-Y') }}" class="form_datetime">
+
+        End:
+        <input size="16" type="text" name="to_date" value="{{ empty(request()->get('to_date'))? \Carbon\Carbon::now()->endOfMonth()->format('d-m-Y') : request()->get('to_date')->format('d-m-Y') }}" class="form_datetime">
+        <input type="submit" value="Submit">
+    </form>
 
     <table id="datatable" class="table table-responsive" id="users-table">
         <thead>
         <tr>
-            <th>id</th>
+            <th>Date</th>
             <th>Breakfast</th>
             <th>Lunch</th>
             <th>Dinner</th>
+            <th>Action</th>
         </tr>
         </thead>
         <tbody>
         @foreach($meal_orders as $meal_order)
                 <tr>
-                    <td>{!! $meal_order->id !!}</td>
+                    <td>{!! $meal_order->created_at->format('d/m/Y') !!}</td>
                     <td>{!! $meal_order->breakfast !!}</td>
                     <td>{!! $meal_order->lunch !!}</td>
                     <td>{!! $meal_order->dinner !!}</td>
+                    <td>
+                        <a href="{!! route('admin.meal_order.edit', $meal_order->id) !!}">Edit</a>
+                    </td>
                 </tr>
         @endforeach
         </tbody>
